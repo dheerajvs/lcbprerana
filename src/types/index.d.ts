f366edc3c173019ff36c1ed8db840b9e3efd0cce
Dheeraj Vepakomma <>
2022-05-21 20:33:47 +0530

    avoid redirection with trailing slash in url

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,7 @@
+// TODO: For some reason, importing astro types in `activities/[...page].astro`
+// is failing the build. So import it here.
+import type { Page, MarkdownInstance } from 'astro'
+
 export type Activity = {
   title: string
   date: string
@@ -6,4 +10,8 @@ export type Activity = {
   location: string
   served: number
   description: string
+}
+
+export interface ActivityPageProps {
+  page: Page<MarkdownInstance<Activity>>
 }
